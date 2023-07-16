cf65513dee0b5e81b9e28f210b743b1efe5ff677
Oleksandra-lab <tka4olex@gmail.com>
2023-07-16 17:49:44 +0300

    Практика  Артемом 15.07.23

---
+++ js/last.js
@@ -0,0 +1,26 @@
+const translations = [
+    {
+      original: "hello",
+      translation: "привіт",
+    },
+    {
+      original: "flower",
+      translation: "квітка",
+    },
+    {
+      original: "apple",
+      translation: "яблуко",
+    },
+  ];
+  
+  function getTranslation(word) {}
+//   Розроби функціонал для вивчення нових англійський слів. У тебе є масив об'єктів translations, де кожен об'єкт це слово (оригінал та переклад).
+
+const list = document.querySelector(".list")
+console.log(list);
+
+function getEnglishVersion() {
+    [...list.children].forEach(element => {
+element.textContent
+    })
+}
---
+++ js/scrpt01.js
@@ -0,0 +1,55 @@
+// task 01
+
+// Потрібно створити картки продуктів
+// const instruments = [{
+//     id: 1,
+//     img: 'https://static.dnipro-m.ua/cache/products/1754/catalog_origin_261194.jpg',
+//     name: 'Молоток',
+//     price: 150
+// }, {
+//     id: 2,
+//     img: 'https://static.dnipro-m.ua/cache/products/5587/catalog_origin_190742.jpg',
+//     name: 'Перфоратор',
+//     price: 3000
+// }, {
+//     id: 3,
+//     img: 'https://static.dnipro-m.ua/cache/products/2299/catalog_origin_261034.jpg',
+//     name: 'Рівень',
+//     price: 2000
+// }]
+
+// // const productList = document.querySelector(".js-productlist");
+// // console.log(productList);
+
+
+// function createMarkup(instruments){
+//     const items = instruments.map(({id, img, name, price}) => {
+//         const li = document.createElement("li");
+//         const image = document.createElement("img");
+//         const h2 = document.createElement("h2");
+//         const h3 = document.createElement("h3");
+//         image.src = img;
+//         image.alt = name;
+//         img.width = 300;
+//         h2.textContent = name;
+//         h3.textContent = price;
+//         li.setAttribute("data-id", id)
+//         li.append(image, h2, h3)
+//         return li
+
+//     })
+//     container.append(...items);
+// }
+// createMarkup(instruments)
+
+// const productList = document.querySelector(".js-productlist");
+// console.log(productList);
+// function createMarkup(arr){
+//     const markup.arr.map(({id, img, name, price}) => {
+// ще раз проглянути і записати
+//     })
+//     console.log(markup);
+// }
+
+// task 2
+
